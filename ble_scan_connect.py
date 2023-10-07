11eb3380fb068e22eabae6bad0d0f04233da3be2
HenryShyu <120787184+HenryShyuHui@users.noreply.github.com>
2023-10-07 16:34:37 +0800

    Update ble_scan_connect.py

--- ble_scan_connect.py
+++ ble_scan_connect.py
@@ -44,9 +44,11 @@ try:
         
 
     ch = dev.getCharacteristics(uuid=UUID(0xfff2))[0]
+    print(ch.propertiesToString())
     cccd_uuid = 0x2902
     cccd_handle = ch.getHandle() + 1  # CCCD handle is the characteristic handle + 1
     dev.writeCharacteristic(cccd_handle, b"\x02\x00")
+    print(ch.propertiesToString())
     if (ch.supportsRead()):
         print (ch.read())
         ch.write("fuck u BLE 0xfff2".encode("utf-8"))
