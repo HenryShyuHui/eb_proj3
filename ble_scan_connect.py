eaf68290d084bb4c2fb6d463d95279045bd070b8
HenryShyu <120787184+HenryShyuHui@users.noreply.github.com>
2023-10-07 15:04:05 +0800

    Update ble_scan_connect.py

--- ble_scan_connect.py
+++ ble_scan_connect.py
@@ -64,7 +64,7 @@ try:
         # print (setValue)
         # print (writeCharacteristic)
 
-    ch = dev.getDescriptors(uuid=UUID(0xfff4))
+    ch = testService.getDescriptors(startHnd=0xfff4, endHnd=0xfff4)
     print (len(ch))
     for k in ch:
         print(k)
