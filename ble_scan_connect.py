aa2f0baab11fdbf7f3f6ee22e4c60f386f77364c
HenryShyu <120787184+HenryShyuHui@users.noreply.github.com>
2023-10-13 18:04:03 +0800

    Update ble_scan_connect.py

--- ble_scan_connect.py
+++ ble_scan_connect.py
@@ -90,7 +90,7 @@ try:
     k = dev.writeCharacteristic(cccd_handle, b"\x01\x00")
     print(ch.propertiesToString())
     print(k)
-    dev.setDelegate(ScanDelegate())
+    dev.setDelegate(MyDelegate())
 
 
     while True:
